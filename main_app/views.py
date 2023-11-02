eb62ecddf48eabbb866a766a64e8374a9a0a9c48
Enes <enesvelovic@gmail.com>
2023-11-02 16:19:14 -0400

    Add Trophy Index URL/View/CSS

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+class Trophy:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, difficulty, description, completion_date):
+    self.name = name
+    self.difficulty = difficulty
+    self.description = description
+    self.completion_date = completion_date
+
+trophies = [
+  Trophy('Lolo', 1, 'Kinda rude.', 3/3/2023),
+  Trophy('Sachi', 2, 'Looks like a turtle.', 3/3/2023),
+  Trophy('Fancy', 3, 'Happy fluff ball.', 3/3/2023),
+  Trophy('Bonk', 4, 'Meows loudly.', 3/3/2023)
+]
+
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def trophy_index(request):
+  return render(request, 'trophies/index.html', { 'trophies': trophies })
